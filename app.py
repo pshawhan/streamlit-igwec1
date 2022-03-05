740fc7ede71228ba9fb9ddcb89a86a92485f9ea1
Peter Shawhan <peter.shawhan@ligo.org>
2022-03-05 15:05:34 -0500

    Initial commit of app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+#-- Peter's first streamlit attempt, with thanks to Jonah for example
+import streamlit as st
+
+gwtc1=pd.read_csv("data/GWTC-1.csv")
+gwtc2=pd.read_csv("data/GWTC-2.csv")
+gwtc21=pd.read_csv("data/GWTC-2.1.csv")
+gwtc21m=pd.read_csv("data/GWTC-2.1-marginal.csv")
+gwtc3=pd.read_csv("data/GWTC-3.csv")
+gwtc3m=pd.read_csv("data/GWTC-3-marginal.csv")
+
+#gwtc = concat([gwtc1,gwtc2,gwtc21,gwtc21m,gwtc3,gwtc3m])
+gwtc = gwtc1
+
+st.write('## GWTC correspondence table')
+st.write("Total {} candidates",format(len(gwtc)))
+st.dataframe(gwtc)
